48deb2a61442edf67ea4c1df3836f134366dd245
pbudiman <Phillipbudiman@gmail.com>
2022-01-17 20:52:56 -0500

    added relevant elements into project component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { HttpClientModule } from '@angular/common/http';
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent]
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../project';
 
 @Component({
   selector: 'app-projects',
@@ -11,5 +12,12 @@ export class ProjectsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  projects: Project[] = [
+    // {title: "title", subtitle: "subtitle", description: "description", link: "link"}, 
+    {title: "password manager", subtitle: "java & javafx", description: "part of a team of three that developed a java-based password manager using AES encryption and a PBKDF2 to generate the encryption key.", link: "https://github.com/phillipbudiman/411_Password_Manager"},
+    {title: "interpreter", subtitle: "ocaml", description: "an interpreter written in OCaml for a basic assembly-like language for a final class project. capabilities include variable storage, logical and mathematical operators, and method declaration/calling.", link: "https://github.com/phillipbudiman/interpreter_ocaml.git"},
+    // {title: "", subtitle: "", description: "", link: ""}
+  ];
 
 }
